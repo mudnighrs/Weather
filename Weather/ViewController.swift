b856eb575ddeae3094bd5597013a204d1311911b
Lakshaya Singh Tanwar <tanwarlakshay21@gmail.com>
2025-07-08 20:15:05 +0530

    Initial commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Lakshaya Singh Tanwar on 03/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  Weather
+//
+//  Created by Lakshaya Singh Tanwar on 03/07/25.
+//
+
+import Foundation
